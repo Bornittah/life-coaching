27847e4a01be4e8cfe4a32dbeff2987298d02b79
bornittah <bornittah@gmail.com>
2022-05-25 22:55:35 +0300

    add dynamic data to home page

--- js/index.js
+++ js/index.js
@@ -21,3 +21,59 @@ const logo = document.querySelector('.logo');
 
 });
 });
+
+let coaches=[
+    {
+        name:"Agasha Bornittah",
+        title:"Senior software Engineer, CEO Empeiha Co. Ltd",
+        profile:"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        photo:"./images/user1.png"
+
+    },
+    {
+        name:"Brennah A",
+        title:"Co founder Digital Emergency",
+        profile:"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        photo:"./images/user1.png"
+
+    },
+    {
+        name:"Grace N",
+        title:"CEO Softtech",
+        profile:"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        photo:"./images/user1.png"
+
+    },
+    {
+        name:"Blair N",
+        title:"Therapist, Incharge Kabwohe Referal Hosp.",
+        profile:"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        photo:"./images/user1.png"
+
+    }
+];
+
+let str='';
+for(let person of coaches){
+    str+=`<div class="card">
+            <div class="card-header">
+                <img src="./images/bg-5.jpg" alt="user" class="bg-image">
+                <img src=${person.photo} alt="user" class="user-image">
+            </div>
+            <div class="card-body">
+                <div  class="card-title">
+                    <h4>${person.name}</h4>
+                    <p class="orange">${person.title}</p>
+                    <hr>
+                </div>
+                <div class="card-description">
+                    <p>
+                    ${person.profile}.
+                    </p>
+                </div>
+            </div>
+        </div>`;
+
+}
+
+document.querySelector('.coaches').innerHTML=str;
